7615cc7fb28058c65c75ba8d062ffcdf24def45a
lorenzo <lrnzmd@gmail.com>
2019-12-10 18:53:31 +0100

    lezioni

--- app/src/main/java/com/example/diarioscuolav4/lezioni/LezioniActivity.kt
+++ app/src/main/java/com/example/diarioscuolav4/lezioni/LezioniActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.diarioscuolav4.R
 
-class lezioni : AppCompatActivity() {
+class LezioniActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/diarioscuolav4/lezioni/LezioniAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.diarioscuolav4.lezioni
+
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.RecyclerView
+
+class LezioniAdapter internal constructor(context: Context) : RecyclerView.Adapter<LezioniAdapter.lezioniHolder>{
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    
+}
